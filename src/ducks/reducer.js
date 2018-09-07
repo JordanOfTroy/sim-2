2b56c7a1188f92591870a716ebf78c4d0c8997cd
Jordan Smithson <jordantroysmithson@gmail.com>
2018-09-07 16:54:33 -0600

    last commit

---
+++ src/ducks/reducer.js
@@ -0,0 +1,17 @@
+initialState = {
+  propertyName: '',
+  address: '',
+  city: '',
+  state: '',
+  zipcode: '',
+  imageURL:''
+}
+/************************************************************* */
+
+/************************************************************* */
+export default function reducer (state=initialState, action) {
+  switch(action.type) {
+    
+    default: return state
+  }
+}
